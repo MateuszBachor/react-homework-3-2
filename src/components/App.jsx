916e698453d38a62b6dd794f2830569496d59051
MateuszBachor <mateuszbachor3@gmail.com>
2022-09-06 00:33:54 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,10 +19,10 @@ export class App extends React.Component {
     isLoad: false,
   };
   fetchImage = async (query, page) => {
-    // const baseUrl = 'https://pixabay.com/api';as
+    // const baseUrl = 'https://pixabay.com/api';
     try {
       const response = await axios.get(
-        `https://pixabay.com/api?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12}`
+        `https://pixabay.com/api?key=28406091-8008b7c1afae3beb3d4e940a7&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12}/`
       );
       return response.data;
     } catch (error) {
